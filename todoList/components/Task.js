5ae5d425d677b3674973f68e11cba86433a44573
Skylar <skylar024@gmail.com>
2024-10-28 18:08:46 -0400

    Setup

---
+++ todoList/components/Task.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const Task = () => {
+
+    return (
+        <View>
+            <Text>This is a Task</Text>
+        </View>
+    )
+}
+
+const styles = Stylesheet.create({
+
+});
+
+export default Task;
