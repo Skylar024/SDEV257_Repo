8d7aa70caa9f5fbe83cacd55e12300a35dceb056
Skylar <skylar024@gmail.com>
2024-11-20 00:27:10 -0500

    Minor Fix

--- Flexbox-Layout/Row.js
+++ Flexbox-Layout/Row.js
@@ -3,7 +3,7 @@ import { PropTypes } from "prop-types";
 import { View } from "react-native";
 import styles from "./styles";
 
-export default function Box({ children }) {
+export default function Row({ children }) {
     return (
         <View style={styles.row}>{children}</View>
     );
